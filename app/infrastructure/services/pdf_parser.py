ffc13c8f3a1f56942e4bf5293f1e7becb815a399
yemi gabriel <yemig@banquemisr.com>
2026-03-28 15:40:31 +0100

    feat(parser): add rapidocr support for deployment

--- app/infrastructure/services/pdf_parser.py
+++ app/infrastructure/services/pdf_parser.py
@@ -1,9 +1,23 @@
-from docling.document_converter import DocumentConverter
+from docling.document_converter import DocumentConverter, PdfFormatOption
+from docling.datamodel.base_models import InputFormat
+from docling.datamodel.pipeline_options import RapidOcrOptions, PdfPipelineOptions
 
 class DoclingParser:
     
     def __init__(self):
         self.converter = DocumentConverter()
+        pipeline_options = PdfPipelineOptions()
+        pipeline_options.do_ocr = True
+        pipeline_options.ocr_options = RapidOcrOptions()
+
+        self.converter = DocumentConverter(
+            format_options={
+                InputFormat.PDF: PdfFormatOption(
+                    pipeline_options=pipeline_options,
+                )
+            }
+        )
+        
         
     def parse(self, document_path: str) -> str:
         """Parse a document and return its content as Document."""
